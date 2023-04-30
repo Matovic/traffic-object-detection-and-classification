33966334bf07db23a4703ba872c7d5c87691163f
JakubHorvat <43437894+JakubHorvat@users.noreply.github.com>
2023-04-30 15:44:57 +0200

    first working prototype

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ import torch
 from albumentations.pytorch import ToTensorV2
 #from utils import seed_everything
 
-DATASET = 'D:/skola/ING/LS2023/NSIETE/zadanie3/NSIETE-zad3/imgs'
+DATASET = '../imgs'
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 # seed_everything()  # If you want deterministic behavior
 NUM_WORKERS = 4
