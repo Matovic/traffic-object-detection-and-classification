3ce6dbeb4b66fcb02b58d8c85b72c5c623b0ae32
Erik Matovič <matovic.er@gmail.com>
2023-05-01 18:32:18 +0200

    Update bbox, TODO: Fix negative transformed values in YOLO

--- src/config.py
+++ src/config.py
@@ -77,6 +77,31 @@ test_transforms = A.Compose(
 )
 
 PASCAL_CLASSES = [
-    "car",
-    "person"
+    "pedestrian",
+    "car", 
+    "traffic light",
+    "sign"
 ]
+
+#PASCAL_CLASSES = [
+#    "aeroplane",
+#    "bicycle",
+#    "bird",
+#    "boat",
+#    "bottle",
+#    "bus",
+#    "car",
+#    "cat",
+#    "chair",
+#    "cow",
+#    "diningtable",
+#    "dog",
+#    "horse",
+#    "motorbike",
+#    "person",
+#    "pottedplant",
+#    "sheep",
+#    "sofa",
+#    "train",
+#    "tvmonitor"
+#]
